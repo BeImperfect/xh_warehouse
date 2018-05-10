12b89cc85879d3d5a52d36ffd1a0613a6bb20ed8
BeImperfect <1900400543@qq.com>
2018-05-10 21:54:48 +0800

    update

--- follow_jd/src/components/loading/index.js
+++
@@ -1,9 +0,0 @@
-const LoadingComponent = require('./Loading.vue')
-
-const loading = {
-    install: Vue => {
-        Vue.component('loading', LoadingComponent)
-    }
-}
-
-module.exports = loading;
